7625d5b4cab1f91f48af5c6e4d0070605d3f0cd4
tatugrigalashvili <146704405+tatugrigalashvili@users.noreply.github.com>
2024-06-17 00:05:14 +0400

    Add files via upload

---
+++ davaleba.js
@@ -0,0 +1,9 @@
+var date = new Date();
+
+console.log(date.getHours());
+console.log(date.getMinutes());
+console.log(date.getSeconds());
+
+document.write(date.getHours() + "<br>");
+document.write(date.getMinutes()+ "<br>");
+document.write(date.getSeconds() + "<br>");
